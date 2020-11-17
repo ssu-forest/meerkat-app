46a45961eb3b4682be501bac9eaa684885f33e81
im-ian <im-ian@naver.com>
2020-11-17 20:47:31 +0900

    init

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, SafeAreaView, View } from 'react-native';
 
-export default function App() {
+import WebView from './screens/webview';
+
+export default () => {
+  const [onLoad, setOnLoad] = React.useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle={'default'} />
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <WebView />
+      </View>
+    </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ screens/webview.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Platform, Dimensions, SafeAreaView, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default () => {
+  const [screenWidth, setScreenWidth] = React.useState(0);
+  const [screenHeight, setScreenHeight] = React.useState(0);
+
+  React.useEffect(() => {
+    const { width, height } = Dimensions.get('screen');
+
+    setScreenWidth(width);
+    setScreenHeight(height);
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <WebView
+          style={{
+            width: screenWidth,
+            height: screenHeight,
+          }}
+          source={{
+            uri: 'https://meerkat-test.herokuapp.com/',
+          }}
+          scalesPageToFit={Platform.OS === 'android'}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
